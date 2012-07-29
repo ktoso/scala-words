d9ad3fe68fb7f9ae46cb29ec9ce017cd33e7c434
Konrad Malawski <konrad.malawski@project13.pl>
2012-07-29 16:15:06 +0200

    tests for WhenWord and implementaiton with Option

--- src/main/scala/pl/project13/scala/words/other/WhenWord.scala
+++ src/main/scala/pl/project13/scala/words/other/WhenWord.scala
@@ -8,6 +8,8 @@ trait WhenWord {
 
   implicit def stringWhen(s: String) = new StringWhen(s)
 
+  implicit def stringWhen[A](s: Option[A]) = new OptionWhen(s)
+
   def blockWithWhen[T](execute: => T) = new HasBlockWithWhen[T](execute)
 
   class DoNotDoBlockUnlessTest[T](block: => T) {
@@ -19,7 +21,11 @@ trait WhenWord {
   }
 
   class StringWhen(s: String) {
-    def when(test: Boolean) = if (test) s else ""
+    def when(test: Boolean): String = if (test) s else ""
+  }
+
+  class OptionWhen[A](o: Option[A]) {
+    def when(test: Boolean): Option[A] = if (test) o else None
   }
 
   class HasBlockWithWhen[T](block: => T) {
