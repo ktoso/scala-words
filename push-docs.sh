19e1a5c141b1741036d9294fc1fd7a80671d0ce1
Konrad Malawski <konrad.malawski@project13.pl>
2012-08-04 23:51:21 +0200

    fix

--- push-docs.sh
+++ push-docs.sh
@@ -12,6 +12,11 @@ git checkout gh-pages
 cp -r docs/_build/html/* .
 rm -rf target/ docs/
 
+mv _static static
+find ./ -type f -exec sed -i ‘s/_static/static/’ {} \;
+mv _sources sources
+find ./ -type f -exec sed -i ‘s/_sources/sources/’ {} \;
+
 git add .
 git ci -m "Updating documentation @ $(date)"
 git push origin gh-pages
